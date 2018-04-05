ba33ee78cdf971ac925f9e78d8ab5544b53d45c0
marcusegues <marcusegues@gmail.com>
2018-04-05 11:23:56 +0200

    About to switch to list of mocked data instead of API calls

--- src/components/ErrorCardList.js
+++ src/components/ErrorCardList.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText,
-} from 'material-ui/Card';
 import CircularProgress from 'material-ui/CircularProgress';
+import { List, ListItem } from 'material-ui/List';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import Subheader from 'material-ui/Subheader';
+import Toggle from 'material-ui/Toggle';
 import { getError, listErrors } from '../api/harry';
 
 class ErrorCardList extends React.Component {
@@ -18,51 +17,87 @@ class ErrorCardList extends React.Component {
   componentDidMount() {
     listErrors().then(data => {
       console.log(data);
-      getError(Object.keys(data)[0]).then(result => {
-        const { create_date, id, label } = result;
-        const { reason, time_detected, times_occurred } = JSON.parse(
-          result.params
-        );
-        this.setState({
-          errors: [
-            ...this.state.errors,
-            {
-              createDate: create_date,
-              id,
-              label,
-              reason,
-              timeDetected: time_detected,
-              timesOccurred: times_occurred,
-            },
-          ],
+      Object.keys(data)
+        .slice(0, 5)
+        .forEach(errorId => {
+          getError(errorId).then(result => {
+            const { create_date, id, label } = result;
+            const { reason, time_detected, times_occurred } = JSON.parse(
+              result.params
+            );
+            this.setState({
+              errors: [
+                ...this.state.errors,
+                {
+                  createDate: create_date,
+                  id,
+                  label,
+                  reason,
+                  timeDetected: time_detected,
+                  timesOccurred: times_occurred,
+                },
+              ],
+            });
+            console.log(result);
+            console.log(JSON.parse(result.params));
+          });
         });
-        console.log(result);
-        console.log(JSON.parse(result.params));
-      });
     });
   }
 
   render() {
     const { errors } = this.state;
+    console.log('Render errors', errors);
     if (!errors.length) {
       return <CircularProgress />;
     }
 
     return (
-      <div>
-        {errors.map(e => (
-          <Card>
-            <CardHeader title={e.label} subtitle={e.reason} />
-            <CardText>Time detected: {e.timeDetected}</CardText>
-            <CardTitle showExpandableButton actAsExpander>
-              Occurrences
-            </CardTitle>
-            <CardText expandable>
-              {e.timesOccurred.map(o => <div>{o}</div>)}
-            </CardText>
-          </Card>
-        ))}
-      </div>
+      <List>
+        <Subheader>Nested List Items</Subheader>
+        <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
+        <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
+        <ListItem
+          primaryText="Inbox"
+          leftIcon={<ContentInbox />}
+          initiallyOpen={true}
+          primaryTogglesNestedList={true}
+          nestedItems={[
+            <ListItem
+              key={1}
+              primaryText="Starred"
+              leftIcon={<ActionGrade />}
+            />,
+            <ListItem
+              key={2}
+              primaryText="Sent Mail"
+              leftIcon={<ContentSend />}
+              disabled={true}
+              nestedItems={[
+                <ListItem
+                  key={1}
+                  primaryText="Drafts"
+                  leftIcon={<ContentDrafts />}
+                />,
+              ]}
+            />,
+            <ListItem
+              key={3}
+              primaryText="Inbox"
+              leftIcon={<ContentInbox />}
+              open={this.state.open}
+              onNestedListToggle={this.handleNestedListToggle}
+              nestedItems={[
+                <ListItem
+                  key={1}
+                  primaryText="Drafts"
+                  leftIcon={<ContentDrafts />}
+                />,
+              ]}
+            />,
+          ]}
+        />
+      </List>
     );
   }
 }
