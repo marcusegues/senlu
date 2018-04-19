cd312eb4c13f24b991498dca1f32bf856a9be627
marcusegues <marcusegues@gmail.com>
2018-04-20 01:05:41 +0200

    Change path

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/degradations', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join('/', 'build', 'index.html'));
 });
 
 app.listen(8080);
