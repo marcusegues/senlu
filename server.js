8f88233891ca32b2f8608e71af1a3a2d00f1ec64
marcusegues <marcusegues@gmail.com>
2018-04-20 09:31:40 +0200

    Revert openapi

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join('/', 'build', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(8080);
